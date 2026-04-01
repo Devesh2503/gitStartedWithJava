f15d60b5dbe56b24e7d76f8265db43aa18ff4925
Devesh Gadhave <deveshgadhave2612@gmail.com>
2026-04-01 21:48:19 +0530

    Initial commit

---
+++ src/com/Kunal/Primitive.java
@@ -0,0 +1,23 @@
+package com.Kunal;
+
+public class Primitive {
+    public static void main(String[] args){
+        int rollno=10;
+
+        char letter='r';
+        float marks= 98.67f;
+        double largeDecimalNumbers=43234321;
+        long largeIntegerValues = 345667787867866L;
+        boolean check= true;
+        boolean flag=false;
+
+
+        // String is not primitive
+        String name="F039109";
+
+        // Wrapper class
+        Integer rno;
+
+
+    }
+}
